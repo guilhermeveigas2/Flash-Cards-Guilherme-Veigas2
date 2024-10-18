ba7f92deafa2f9e0b2d0c6f1e03ba3c7b576d280
guilhermeveigas2 <146458725+guilhermeveigas2@users.noreply.github.com>
2024-10-18 12:26:30 -0300

    18/10

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Categoria',
+    'Quem foi o maior jogador de futebol do brasil?',
+    'Edson Arantes do Nascimento(Pelé)'
+)
+
+criaCartao(
+    'Categoria',
+    'Qual pais ganhou a primeira copa do mundo?',
+    'Foi o Uruguai'
+)
+
+criaCartao(
+    'Categoria',
+    'Quais países ganharam a copa do mundo?',
+    'R:Brasil, Alemanha, Itália, Argentina, França, Uruguai e Espanha'
+)
+
+criaCartao(
+    'Categoria',
+    'Pergunta?',
+    'Resposta'
+)
